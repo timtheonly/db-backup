8f1a376cd8ff15ca6aca52f1f2a4c823dae8079b
daniel hogan <theonlytim@hotmail.com>
2012-10-20 17:01:14 +0100

    added main files

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bkpbtn = new System.Windows.Forms.Button();
+            this.outputlabel = new System.Windows.Forms.Label();
+            this.choosefolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.choosedirbtn = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // bkpbtn
+            // 
+            this.bkpbtn.Location = new System.Drawing.Point(254, 115);
+            this.bkpbtn.Name = "bkpbtn";
+            this.bkpbtn.Size = new System.Drawing.Size(75, 69);
+            this.bkpbtn.TabIndex = 1;
+            this.bkpbtn.TabStop = false;
+            this.bkpbtn.Text = "Backup";
+            this.bkpbtn.UseVisualStyleBackColor = true;
+            this.bkpbtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // outputlabel
+            // 
+            this.outputlabel.BackColor = System.Drawing.Color.White;
+            this.outputlabel.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.outputlabel.Location = new System.Drawing.Point(12, 9);
+            this.outputlabel.Name = "outputlabel";
+            this.outputlabel.Size = new System.Drawing.Size(236, 198);
+            this.outputlabel.TabIndex = 2;
+            // 
+            // choosefolderDialog
+            // 
+            this.choosefolderDialog.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.choosefolderDialog.ShowNewFolderButton = false;
+            // 
+            // choosedirbtn
+            // 
+            this.choosedirbtn.Location = new System.Drawing.Point(254, 40);
+            this.choosedirbtn.Name = "choosedirbtn";
+            this.choosedirbtn.Size = new System.Drawing.Size(75, 69);
+            this.choosedirbtn.TabIndex = 3;
+            this.choosedirbtn.Text = "Choose folder";
+            this.toolTip.SetToolTip(this.choosedirbtn, "choose folder to backup");
+            this.choosedirbtn.UseVisualStyleBackColor = true;
+            this.choosedirbtn.Click += new System.EventHandler(this.choosedirbtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(341, 216);
+            this.Controls.Add(this.choosedirbtn);
+            this.Controls.Add(this.outputlabel);
+            this.Controls.Add(this.bkpbtn);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "dropbox backup";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bkpbtn;
+        private System.Windows.Forms.Label outputlabel;
+        private System.Windows.Forms.FolderBrowserDialog choosefolderDialog;
+        private System.Windows.Forms.Button choosedirbtn;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Ionic.Zip;
+using DropNet;
+//using IniParser;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private  string output;
+        private string bkuploc, zipfileName;
+        private bool newUser;
+        private DropNetClient dbclient;
+        public Form1()
+        {
+            InitializeComponent();
+            //set default backup location
+            bkuploc = "U:\\";
+            output = "Click backup to begin.....";
+            outputlabel.Text = output;
+            dbclient = new DropNetClient("************", "**********");
+            newUser = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            output += "\nBackup initiated (This may take a while).... \n backing up:"+ bkuploc +" ... \n";
+            bkpbtn.Enabled = false;
+            choosedirbtn.Enabled = false;
+            outputlabel.Text = output;
+            zipUp();
+            output += "\nZip completed, now attepmting to send backup to Dropbox...";
+            outputlabel.Text = output;
+            auththenicateWithDropbox();
+            sendBackuptodb();
+            output += "\nComplete; backup sent to Dropbox...";
+            outputlabel.Text = output;
+            cleanup();
+            output += "\nYou can now close this window....";
+            outputlabel.Text = output;
+        }
+        
+        private void choosedirbtn_Click(object sender, EventArgs e)
+        {
+            if (choosefolderDialog.ShowDialog() == DialogResult.OK)
+            {
+                bkuploc = choosefolderDialog.SelectedPath;
+            }
+
+        }
+
+        private void zipUp()
+        {
+            try
+            {
+                ZipFile myzip = new ZipFile();
+                myzip.AddDirectory(bkuploc);
+                zipfileName= "Backup-" + System.DateTime.Now.ToString("d-M-yy--HH-m-s") + ".zip";
+                myzip.Save(zipfileName);   
+            }
+            catch (System.Exception except1)
+            {
+                output += "\n" + except1.Message;
+                outputlabel.Text = output;
+            }
+        }
+        
+        private void auththenicateWithDropbox()
+        {
+            string user_token ="", user_secret="";
+            /*if user has run a backup before read the access token from config.ini
+             otherwise get the access token and then save it to config.ini*/
+            if (File.Exists("config.ini"))
+            {
+                StreamReader str = new StreamReader("config.ini");
+                user_token = str.ReadLine();
+                user_secret = str.ReadLine();
+                str.Close();
+            }
+            else
+            {
+                newUser = true;
+                string url = dbclient.GetTokenAndBuildUrl();
+                System.Diagnostics.Process.Start(url);
+                //wait for the user to accept the app on dropbox
+                MessageBox.Show("click ok when you have authenicated with dropbox","Authtenticate with dropbox");
+
+                var accesstoken = dbclient.GetAccessToken();
+                //save the access token for future use
+                StreamWriter stw = new StreamWriter("config.ini");
+                stw.WriteLine(accesstoken.Token);
+                stw.WriteLine(accesstoken.Secret);
+                stw.Close();
+            }
+            dbclient.UserLogin = new DropNet.Models.UserLogin {Token = user_token, Secret = user_secret};
+        }
+
+        private void sendBackuptodb()
+        {
+            try
+            {
+                if (newUser)
+                {
+                    //create a sub dir called backups to store the zip files
+                    dbclient.CreateFolder("/backups");
+                }
+                byte[] bytes = File.ReadAllBytes(zipfileName);
+                var uploaded = dbclient.UploadFilePUT("/backups/" + zipfileName, zipfileName, bytes);
+            }
+            catch (DropNet.Exceptions.DropboxException dbexcept1)
+            {
+                output += "Error sending backup to dropbox:\n" + dbexcept1.Message;
+                outputlabel.Text = output;
+            }
+        }
+
+        private void cleanup()
+        {
+            try
+            {
+                File.Delete(zipfileName);
+            }
+            catch (System.SystemException except2)
+            {
+                //not fatal but looks bad
+                output += "Error deleteing temporary zip archive:\n" + except2.Message.ToString();
+            }
+        }
+        
+
+    }
+}
